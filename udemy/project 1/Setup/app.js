22d95384815dd57fc5493a0ed65f402cc849adeb
Zuza Zuber <zuberzuzanna@gmail.com>
2018-10-17 12:33:52 +0200

    finish game

--- udemy/project 1/Setup/app.js
+++ udemy/project 1/Setup/app.js
@@ -3,30 +3,54 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameOn: false
+    gameOn: false,
+    turns: []
   },
   methods: {
     startGame: function() {
       this.gameOn = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+      this.turns = [];
     },
     attack: function() {
-      this.monsterHealth -= this.calculateDamage(3, 10);
+      let damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
       if(this.checkWin()) {
         return;
       }
-      this.playerHealth -= this.calculateDamage(5,12);
-      this.checkWin();
+      this.monsterDamage();
     },
     specialAttack: function() {
-
+      let damage = this.calculateDamage(3, 10);
+      this.monsterHealth -= damage;
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage
+      });
+      if(this.checkWin()) {
+        return;
+      }
+      this.monsterDamage();
     },
     heal: function() {
-
+      if (this.playerHealth <= 90){
+        this.playerHealth += 10;
+      } else {
+        this.playerHealth = 100;
+      }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10'
+      });
+      this.monsterDamage();
     },
     giveUp: function() {
-
+      this.gameOn = false;
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -48,6 +72,15 @@ new Vue({
         return true;
       }
       return false;
+    },
+    monsterDamage: function() {
+      let damage = this.calculateDamage(5,12);
+      this.playerHealth -= damage;
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage
+      });
+      this.checkWin();
     }
   }
 });
